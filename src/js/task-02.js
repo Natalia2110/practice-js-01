810c0ebca91460995d0e2d53dc370e3e8e3df050
Natalia Yakovyna <yakovina.n13@gmail.com>
2023-11-07 23:52:51 +0200

    task02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,35 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.getElementById('ingredients');
+// console.log(ulEl);
+
+// ingredients.forEach(ingredient => {
+//   const liEl = document.createElement('li');
+//   liEl.textContent = ingredient;
+//   liEl.classList.add('item');
+
+//   console.log(liEl);
+// ulEl.prepend(liEl);
+
+// });
+
+// const itemEl = ingredients
+//   .map(ingredient => `<li class = "item">${ingredient}</li>`)
+//   .join("");
+//   // console.log(itemEl);
+
+//   ulEl.insertAdjacentHTML('afterbegin', itemEl);
+//   console.log(ulEl);
+
+
+  const itemElement = ingredients.reduce((murkup, ingredient) => 
+    murkup + `<li class = "item">${ingredient}</li>`
+  , "");
+  ulEl.insertAdjacentHTML('afterbegin', itemElement);
+  console.log(ulEl);
+
+  
+
+  
