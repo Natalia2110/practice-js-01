dd0451aa0d1e5133afd4c72c4228e472dfbbc606
Natalia Yakovyna <yakovina.n13@gmail.com>
2023-12-18 13:49:00 +0200

    task-07

---
+++ src/js/task-07.js
@@ -0,0 +1,17 @@
+const refs = {
+    inputEl: document.getElementById('font-size-control'),
+    spanEl: document.getElementById('text'),
+};
+
+// console.log(refs.inputEl);
+// console.log(refs.spanEl);
+
+refs.inputEl.addEventListener("input", onInputChange);
+
+function onInputChange (event) {
+    // console.log(event.currentTarget.value);
+
+    refs.spanEl.style.fontSize = `${Number(event.currentTarget.value)}px`;
+    // console.log(refs.spanEl.style.fontSize);
+}
+
