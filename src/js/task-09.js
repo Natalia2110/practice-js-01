cc3d477dfb17725bdcdd9d97f309832ac3c76de5
Natalia Yakovyna <yakovina.n13@gmail.com>
2023-12-19 13:21:19 +0200

    task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,23 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  spanEl: document.querySelector('.color'),
+  btnEl: document.querySelector('.change-color'),
+}
+
+// console.log(refs.bodyEl);
+// console.log(refs.spanEl);
+// console.log(refs.btnEl);
+
+refs.btnEl.addEventListener('click', handleBtnElClick);
+
+function handleBtnElClick(event) {
+  const changeColor = getRandomHexColor();
+  refs.spanEl.textContent = changeColor;
+  refs.bodyEl.style.backgroundColor =changeColor; 
+  // console.log(changeColor);
+
+}
